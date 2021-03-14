40f76e9e0d3d53bac3a5a899f5c93b9136c1eaaa
Guilherme Maciel <67983309+GuilhermeMacielJs@users.noreply.github.com>
2021-03-14 02:39:49 -0300

    Projeto listagem de repositório concluído.
    Foi utilizado no projeto os seguintes pacotes e bibliotecas:
    
    -Babel
    -Webpack
    -react
    -react-dom
    -scss

--- aulas/01-github-explorer/src/components/RepositoryList.jsx
+++ aulas/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,30 +1,26 @@
-export function RepositoryList(){
-    return(
+import { RepositoryItem } from "./RepositoryItem"
+
+import  '../styles/repositories.scss'
+import { useState, useEffect } from "react"
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() =>{
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json()
+        .then(data => setRepositories(data))
+        )
+    },[])
+
+    return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
-            <u>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Acessar repositório
-                    </a>
-                </li>
-            </u>
+            <ul>
+                {repositories.map(repository => {
+                    return  <RepositoryItem key={repository.name} repository={repository}/>
+                })}
+            </ul>
         </section>
     )
 }
